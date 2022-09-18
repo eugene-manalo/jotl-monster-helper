41724c17b750e38b984fa8f6903253dcf0453482
Eugene Manalo <eugene.manalo@amaysim.com.au>
2022-09-18 09:38:30 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import AttackModifier from './components/cards/AttackModifier.vue'
+</script>
+
+<template>
+    <div class="draw-pile">
+        <AttackModifier />
+    </div>
+
+</template>
+
+<style scoped>
+/* header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  } */
+</style>
---
+++ src/components/cards/AttackModifier.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <img src="@/assets/images/attack_modifier_back.png" alt="attack_modifier_back"/>
+        <img src="@/assets/images/attack_modifier_2x.png" />
+    </div>
+    <div class="actions">
+        <button>Draw</button>
+    </div>
+</template>
+
+<style scoped>
+    .card img {
+        width: 179px;
+        height: 120px;
+        margin: 5px;
+    }
+    
+    .actions {
+        margin: 5px;
+    }
+</style>
